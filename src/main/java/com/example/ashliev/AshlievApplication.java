2ded4a4f592cbdc995a0922de7c0d218e060535f
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-19 05:09:42 +0530

    Initial commit

---
+++ src/main/java/com/example/ashliev/AshlievApplication.java
@@ -0,0 +1,13 @@
+package com.example.ashliev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshlievApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshlievApplication.class, args);
+	}
+
+}
